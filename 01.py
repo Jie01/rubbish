f7dd2941fca601f5e6b066f30024dc06b98b84b3
Jie01 <aukitty0504@gmail.com>
2021-01-23 18:30:59 +0800

    add Taiwan and UK

--- 01.py
+++ 01.py
@@ -1,8 +1,14 @@
 country = input("your country : ")
 age = int(input('your age : '))
 
-if country == '台灣':
+if country == '台灣' | country == "Taiwan":
     if age >= 18:
         print("you can drive")
     else:
+        print("you can't drive")
+
+elif country == "美國" | country == "US":
+    if age >= 16:
+        print("you can drive")
+    else: 
         print("you can't drive")
